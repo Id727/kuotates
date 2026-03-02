922da33e6bbebd8310171c944ca79c1b07b4759b
Id727 <rian012529@gmail.com>
2026-03-02 17:49:17 +0800

    Update availability status for several products

--- product-data.js
+++ product-data.js
@@ -12,9 +12,9 @@ const products = {
     ],
     xl: [
         { name: ' SUPERMINI', price: 39000, desc: '❌ Habis | Masa aktif: 27/28 hari<br>- AREA 1: 13–15 GB<br>- AREA 2: 15–17 GB<br>- AREA 3: 20–22 GB<br>- AREA 4: 30–32 GB' },
-        { name: ' MINI', price: 50000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 32–34 GB<br>- AREA 2: 34–36 GB<br>- AREA 3: 39–41 GB<br>- AREA 4: 49–51 GB' },
+        { name: ' MINI', price: 50000, desc: '❌ Habis | Masa aktif: 27/28 hari<br>- AREA 1: 32–34 GB<br>- AREA 2: 34–36 GB<br>- AREA 3: 39–41 GB<br>- AREA 4: 49–51 GB' },
         { name: ' BIG', price: 55000, desc: '❌ Habis | Masa aktif: 27/28 hari<br>- AREA 1: 38–40 GB<br>- AREA 2: 40–42 GB<br>- AREA 3: 45–47 GB<br>- - AREA 4: 55–57 GB' }, 
-        { name: ' JUMBO V2', price: 63000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 50–53 GB<br>- AREA 2: 52–55 GB<br>- AREA 3: 57–60 GB<br>- AREA 4: 67–70 GB' },
+        { name: ' JUMBO V2', price: 63000, desc: '❌ Habis | Masa aktif: 27/28 hari<br>- AREA 1: 50–53 GB<br>- AREA 2: 52–55 GB<br>- AREA 3: 57–60 GB<br>- AREA 4: 67–70 GB' },
         { name: ' BIG SUPER', price: 71000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 63–65 GB<br>- AREA 2: 65–67 GB<br>- AREA 3: 70–72 GB<br>- AREA 4: 80–82 GB' },
         { name: ' JUMBO', price: 83000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 65 GB<br>- AREA 2: 70 GB<br>- AREA 3: 83 GB<br>- AREA 4: 123 GB' },
         { name: ' JUMBO V3', price: 88000, desc: '✅ Ready Stock | Masa aktif: 27/28 hari<br>- AREA 1: 87–90 GB<br>- AREA 2: 89–92 GB<br>- AREA 3: 94–97 GB<br>- AREA 4: 104–107 GB' }
